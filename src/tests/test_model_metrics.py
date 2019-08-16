0d962fd6338ac299090ca08fd798080f652beaca
David Tan <davidtan@thoughtworks.com>
2019-08-16 15:13:10 +0800

    Add functional tests to verify that precision and recall are above threshold

---
+++ src/tests/test_model_metrics.py
@@ -0,0 +1,21 @@
+import unittest 
+from sklearn.metrics import precision_score, recall_score
+
+from src.train import prepare_data_and_train_model
+
+class TestModelMetrics(unittest.TestCase):
+  def test_model_precision_score_should_be_above_threshold(self):
+    model, X_test, Y_test = prepare_data_and_train_model()
+    Y_pred = model.predict(X_test)
+
+    precision = precision_score(Y_test, Y_pred)
+
+    self.assertGreaterEqual(precision, 0.7)
+
+  def test_model_recall_score_should_be_above_threshold(self):
+    model, X_test, Y_test = prepare_data_and_train_model()
+    Y_pred = model.predict(X_test)
+
+    recall = recall_score(Y_test, Y_pred)
+
+    self.assertGreaterEqual(recall, 0.6)
--- src/train.py
+++ src/train.py
@@ -1,25 +1,32 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 from preprocessing import (add_categorical_columns, add_derived_title,
                            add_is_alone_column, categorize_column, impute_nans, 
                            train_model)
-                           
-df = pd.read_csv("./input/train.csv")
 
-df = impute_nans(df, categorical_columns=[
-                'Embarked'], continuous_columns=['Fare', 'Age'])
-df = add_derived_title(df)
-df = add_is_alone_column(df)
-df = add_categorical_columns(df)
+def prepare_data_and_train_model():
+    df = pd.read_csv("./input/train.csv")
 
-df = df.drop(['Parch', 'SibSp', 'Name', 'PassengerId',
-            'Ticket', 'Cabin'], axis=1)
+    df = impute_nans(df, categorical_columns=[
+                    'Embarked'], continuous_columns=['Fare', 'Age'])
+    df = add_derived_title(df)
+    df = add_is_alone_column(df)
+    df = add_categorical_columns(df)
 
-Y = df["Survived"]
-X = df.drop("Survived", axis=1)
+    df = df.drop(['Parch', 'SibSp', 'Name', 'PassengerId',
+                'Ticket', 'Cabin'], axis=1)
 
-rf_model, accuracy_random_forest = train_model(
-    RandomForestClassifier, X, Y, n_estimators=100)
+    Y = df["Survived"]
+    X = df.drop("Survived", axis=1)
 
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+    rf_model, accuracy_random_forest = train_model(
+        RandomForestClassifier, X_train, Y_train, n_estimators=100)
+
+    return rf_model, X_test, Y_test
+
+model, X_test, Y_test = prepare_data_and_train_model()
 # if necessary, pickle rf_model for further testing / deployment
