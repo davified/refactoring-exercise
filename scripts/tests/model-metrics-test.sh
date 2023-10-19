91bf03cf24e194f42ee5ecbebe0fefae6ea393c5
David Tan <davidtan@thoughtworks.com>
2023-10-19 16:48:58 +1100

    Run tests with coverage

--- scripts/tests/model-metrics-test.sh
+++ scripts/tests/model-metrics-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-python -m pytest -rA tests/test_model_metrics.py
+python -m pytest -rA --cov-report term tests/test_model_metrics.py
 
 # -r displays extra test summary info and test logs
 # -A configures -r for (A)ll tests, or (f)ailed, (E)rror, (s)kipped, (x)failed, (X)passed, (p)assed, (P)assed with output, (a)ll except passed (p/P) tests
